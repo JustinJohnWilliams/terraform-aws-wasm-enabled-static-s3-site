6a10698b751cd636dabde9c29dfca5fe9994235c
Justin Williams <justinjohnwilliams@gmail.com>
2023-09-19 20:20:40 -0500

    added bucket module

---
+++ modules/main.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_version = ">= 1.2.9"
+  backend "s3" {}
+}
+
+locals {
+  tags = merge(var.default_tags, var.tags)
+}
+
+module "bucket" {
+  source = "./bucket"
+
+  bucket_name    = var.bucket_name
+  aws_account_id = var.aws_account_id
+  tags           = local.tags
+}
