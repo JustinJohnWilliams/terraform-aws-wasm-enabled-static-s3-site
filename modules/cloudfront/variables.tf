70526c3528b106a5e4d8a004fcf77af39300244f
Justin Williams <justinjohnwilliams@gmail.com>
2023-09-20 15:27:44 -0500

    added cloudfront module

---
+++ modules/cloudfront/variables.tf
@@ -0,0 +1,17 @@
+# ---------------------------------------------------------------------------------------------------------------------
+# REQUIRED PARAMETERS
+# You must provide a value for each of these parameters.
+# ---------------------------------------------------------------------------------------------------------------------
+variable "bucket_domain" {
+  description = "the bucket domain to point the cf distro to"
+  type        = string
+}
+
+variable "lambda_arn" {
+  description = "the lambda for the cache policy"
+  type        = string
+}
+
+variable "tags" {
+  type = map(any)
+}
