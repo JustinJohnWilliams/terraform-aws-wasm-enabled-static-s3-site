a72d193d8c7d966cac2966b4af4c73a14c4e27fe
Justin Williams <justinjohnwilliams@gmail.com>
2023-09-19 21:54:45 -0500

    added lambda module

---
+++ modules/lambda/main.tf
@@ -0,0 +1,24 @@
+locals {
+  lambda_role_name = "${var.function_name}-lambda-role"
+  lambda_zip_file  = "${path.module}/lambda.zip"
+}
+
+resource "aws_lambda_function" "lambda" {
+  provider = aws.east
+
+  function_name    = var.function_name
+  filename         = local.lambda_zip_file
+  role             = "arn:aws:iam::802818746700:role/service-role/amir-sucks-he-really-really-sucks-func-role-9epx0xmv" #TOD: add module for IAM
+  handler          = "index.handler"
+  source_code_hash = filebase64sha256(local.lambda_zip_file)
+
+  runtime     = "nodejs18.x"
+  timeout     = 3
+  memory_size = 128
+
+  lifecycle {
+    ignore_changes = [source_code_hash, handler, environment]
+  }
+
+  tags = var.tags
+}
