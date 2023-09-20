5560ae14486235c573bdc4dfeab943fad574b16d
Justin Williams <justinjohnwilliams@gmail.com>
2023-09-20 16:52:32 -0500

    make it purdy

--- modules/outputs.tf
+++ modules/outputs.tf
@@ -1,3 +1,11 @@
-output "distro" {
-  url = module.cloudfront.cloudfront.domain_name
+output "bucket_info" {
+  value = module.bucket.bucket_info
+}
+
+output "lambda_info" {
+  value = module.lambda.lambda
+}
+
+output "url" {
+  value = module.cloudfront.cloudfront.domain_name
 }
