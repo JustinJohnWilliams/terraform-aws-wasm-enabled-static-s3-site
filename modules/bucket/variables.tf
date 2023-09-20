0ed39d25500540af4665b090d69efb1f356daee1
Justin Williams <justinjohnwilliams@gmail.com>
2023-09-19 20:20:40 -0500

    added bucket module

---
+++ modules/bucket/variables.tf
@@ -0,0 +1,22 @@
+# ---------------------------------------------------------------------------------------------------------------------
+# REQUIRED PARAMETERS
+# You must provide a value for each of these parameters.
+# ---------------------------------------------------------------------------------------------------------------------
+variable "bucket_name" {
+  description = "the name of the s3 bucket to hold the static content"
+  type        = string
+}
+
+variable "aws_account_id" {
+  description = "The AWS Account ID to deploy the resources to"
+  type        = string
+}
+
+variable "site_index_page" {
+  description = "Specify the home or default page of the website"
+  type        = string
+}
+
+variable "tags" {
+  type = map(any)
+}
