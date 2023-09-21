9a8df32a9a2e7db8253c1b1d55f2e8757c92f1b5
Justin Williams <justinjohnwilliams@gmail.com>
2023-09-21 12:34:37 -0500

    with bucket - removed server side encryption all together

--- modules/bucket/main.tf
+++ modules/bucket/main.tf
@@ -18,15 +18,6 @@ resource "aws_s3_bucket_versioning" "bucket_versioning" {
   }
 }
 
-resource "aws_s3_bucket_server_side_encryption_configuration" "bucket_sse" {
-  bucket = aws_s3_bucket.bucket.id
-  rule {
-    apply_server_side_encryption_by_default {
-      sse_algorithm = "AES256"
-    }
-  }
-}
-
 resource "aws_s3_bucket_public_access_block" "public_access_block" {
   bucket = aws_s3_bucket.bucket.id
 
