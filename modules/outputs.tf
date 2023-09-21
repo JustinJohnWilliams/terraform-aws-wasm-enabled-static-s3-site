16a6321ad729d465a7cfeb37cd3151432ef036ca
Justin Williams <justinjohnwilliams@gmail.com>
2023-09-20 21:34:51 -0500

    with purdy

--- modules/outputs.tf
+++ modules/outputs.tf
@@ -1,3 +1,11 @@
+output "bucket_info" {
+  value = {
+    name             = module.bucket.bucket
+    domain_name      = module.bucket.bucket_domain_name
+    website_endpoint = module.bucket.website_endpoint
+  }
+
+}
 output "url" {
   value = module.cloudfront.cloudfront.domain_name
 }
