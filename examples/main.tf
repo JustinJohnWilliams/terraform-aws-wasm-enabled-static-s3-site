804f8c516e88b30c4bf26c3fd129525819aa6d2c
Justin Williams <justinjohnwilliams@gmail.com>
2023-10-01 21:16:55 -0500

    added example

---
+++ examples/main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_version = ">= 1.2.9"
+}
+
+locals {
+  aws_acount_id = ""
+  env           = "sandbox"
+  region        = "us-east-2"
+  tags = {
+    "terraform-manaed" = true
+  }
+}
+
+module "example_site" {
+  source = "../modules"
+
+  project_name = "my_awesome_site"
+  zip_dir      = "./site.zip"
+
+  aws_account_id = local.aws_acount_id
+  region         = local.region
+  env            = local.env
+  default_tags   = local.tags
+}
