cbfd09908cf72a136c93a071dac59a6cddaaabee
Justin Williams <justinjohnwilliams@gmail.com>
2024-01-09 17:04:00 -0600

    added ttl to cloudfront

--- modules/cloudfront/main.tf
+++ modules/cloudfront/main.tf
@@ -39,8 +39,8 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
 
     viewer_protocol_policy = "redirect-to-https"
     min_ttl                = 0
-    #default_ttl            = 3600
-    #max_ttl                = 86400
+    default_ttl            = 3600
+    max_ttl                = 86400
 
     lambda_function_association {
       event_type   = "origin-response"
