bdfd8d31de33f79cc55ef3cadbae5db8b1223059
Justin Williams <justinjohnwilliams@gmail.com>
2024-01-09 11:32:46 -0600

    added gh example for amir

---
+++ examples/gh_example/main.tf
@@ -0,0 +1,18 @@
+locals {
+  aws_acount_id = ""
+  region        = "us-east-2"
+}
+
+module "example_site" {
+  source = "git@github.com:JustinJohnWilliams/wasm-enabled-static-s3-site-module.git//modules?ref=v1.0.8"
+
+  project_name = "my.awesome.site"
+  zip_dir      = "./site.zip"
+
+  aws_account_id = local.aws_acount_id
+  region         = local.region
+}
+
+output "foo" {
+  value = module.example_site
+}
