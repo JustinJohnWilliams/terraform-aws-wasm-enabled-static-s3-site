14a208af568d040a3cce86f5787fb473847db21c
Justin Williams <justinjohnwilliams@gmail.com>
2024-01-09 17:08:38 -0600

    updated local example with force bucket destroy

--- examples/local_example/main.tf
+++ examples/local_example/main.tf
@@ -6,9 +6,15 @@ locals {
 module "example_site" {
   source = "../../"
 
-  project_name = "my_awesome_site"
+  project_name = "myawesome.site.org"
   zip_dir      = "../site.zip"
 
   aws_account_id = local.aws_acount_id
   region         = local.region
+
+  force_bucket_destroy = true
+}
+
+output "data" {
+  value = module.example_site
 }
