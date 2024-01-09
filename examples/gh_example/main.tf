aa7562b2f2d99d9730b9ca255e88ec57f58db5fc
Justin Williams <justinjohnwilliams@gmail.com>
2024-01-09 12:05:46 -0600

    updated example to have force_bucket_destroy

--- examples/gh_example/main.tf
+++ examples/gh_example/main.tf
@@ -4,13 +4,15 @@ locals {
 }
 
 module "example_site" {
-  source = "git@github.com:JustinJohnWilliams/wasm-enabled-static-s3-site-module.git//modules?ref=v1.0.8"
+  source = "git@github.com:JustinJohnWilliams/wasm-enabled-static-s3-site-module.git//modules?ref=v1.0.12"
 
   project_name = "my.awesome.site"
   zip_dir      = "./site.zip"
 
   aws_account_id = local.aws_acount_id
   region         = local.region
+
+  force_bucket_destroy = true
 }
 
 output "foo" {
